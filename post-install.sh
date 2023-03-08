5351975ad263cd97ebc78aa9eb0aa818b0e934d1
Bysholo <89215981+Bysholo@users.noreply.github.com>
2023-03-08 03:46:34 -0300

    Add files via upload

--- post-install.sh
+++ post-install.sh
@@ -2,6 +2,7 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install snapd flatpak -y
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 
 sudo snap remove firefox -y
 sudo add-apt-repository ppa:mozillateam/ppa
@@ -16,10 +17,11 @@ Pin-Priority: 1001
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
 sudo apt-get install firefox plank tilix steam vlc neofetch neovim -y
-sudo snap install onlyoffice-ds simplenote spotify -y
-sudo flatpak install org.discordapp.Discord org.kryogenix.Pick -y
+sudo snap install onlyoffice-ds simplenote spotify
+sudo flatpak install org.discordapp.Discord org.kryogenix.Pick
 cd ~/Downloads/
 sudo tar -xf fonts.tar.gz --directory ~/.fonts/
-sudo tar -xf themes.tar.gz --directory ~/.themes/
 sudo cp -r wp ~/Pictures/
 sudo mv wp /etc/
+cd ./WhiteSur-gtk-theme/
+./install.sh/

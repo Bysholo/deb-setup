537273a6d9847d021b8174e7e622855e21fff960
Bysholo <89215981+Bysholo@users.noreply.github.com>
2023-03-09 00:06:45 -0300

    Add files via upload

--- post-install.sh
+++ post-install.sh
@@ -4,7 +4,7 @@ sudo apt-get upgrade -y
 sudo apt-get install snapd flatpak -y
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git
 
-sudo snap remove firefox -y
+sudo snap remove firefox
 sudo add-apt-repository ppa:mozillateam/ppa
 sudo apt-get update -y
 
@@ -16,10 +16,10 @@ Pin-Priority: 1001
 
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
-sudo apt-get install firefox plank tilix steam vlc neofetch neovim -y
+sudo apt-get install firefox plank tilix steam vlc neofetch neovim virtualbox -y
 sudo snap install onlyoffice-ds simplenote spotify
 sudo flatpak install org.discordapp.Discord org.kryogenix.Pick
-cd ~/Downloads/
+cd ~/Downloads/deb-setup/
 sudo tar -xf fonts.tar.gz --directory ~/.fonts/
 sudo cp -r wp ~/Pictures/
 sudo mv wp /etc/
